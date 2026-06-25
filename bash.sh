b875d2a20b73896d2ee700c17b939b3381a302b9
Parisa Moosavinezhad <parisa.moosavinezhad@cloudflight.io>
2026-06-25 10:11:17 +0000

    updated

---
+++ bash.sh
@@ -0,0 +1,27 @@
+````bash
+python3 -m venv .venv
+ 
+Set-ExecutionPolicy Bypass -Scope Process -Force
+ 
+# Activate environment
+.\.venv\Scripts\Activate.ps1
+ 
+pip install fastapi[standard]
+ 
+python -m venv .venv
+ 
+deactivate
+ 
+chmod +x .venv/bin/activate
+ 
+.venv/bin/activate
+ 
+chmod +x .venv/bin/activate && source .venv/bin/activate
+ 
+pip install -r requirements.txt
+ 
+pip install ipykernel
+ 
+python -m ipykernel install --user --name=.venvp --display-name ".venvp"
+
+pip install uv
