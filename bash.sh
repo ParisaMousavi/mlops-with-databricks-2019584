73def8c3dd0d097ada615a814df1bc1cf1d7b9c1
Parisa Moosavinezhad <parisa.moosavinezhad@cloudflight.io>
2026-06-26 08:38:18 +0000

    updated gitignore

---
+++ bash.sh
@@ -0,0 +1,40 @@
+````bash
+python3 -m venv .venv
+ 
+Set-ExecutionPolicy Bypass -Scope Process -Force
+ 
+# Activate environment
+.\.venv\Scripts\Activate.ps1
+ 
+pip install fastapi[standard]
+ 
+python -m venv .venv
+ 
+deactivate
+ 
+chmod +x .venv/bin/activate
+ 
+.venv/bin/activate
+ 
+chmod +x .venv/bin/activate && source .venv/bin/activate
+ 
+pip install -r requirements.txt
+ 
+pip install ipykernel
+ 
+python -m ipykernel install --user --name=.venvp --display-name ".venvp"
+
+pip install uv
+
+uv pip install -r pyproject.toml --all-extras
+
+uv lock
+
+
+git remote add upstream https://github.com/LinkedInLearning/mlops-with-databricks-2019584.git
+
+git fetch upstream 02_03
+
+git checkout -b 02_02
+
+git checkout main
